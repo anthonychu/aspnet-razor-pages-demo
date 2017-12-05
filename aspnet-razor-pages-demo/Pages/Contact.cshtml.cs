5dcbca63a4e65694ba91b790bc0fa6dfbc79466c
Anthony Chu <anthony@anthonychu.ca>
2017-12-04 17:57:47 -0800

    Add project files.

---
+++ aspnet-razor-pages-demo/Pages/Contact.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_razor_pages_demo.Pages
+{
+    public class ContactModel : PageModel
+    {
+        [BindProperty]
+        public Contact Contact { get; set; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // save to database
+
+            Message = "Thanks for signing up for our newsletter!";
+
+            return RedirectToPage();
+        }
+    }
+}
+
+public class Contact
+{
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+}
---
+++ aspnet-razor-pages-demo/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspnet_razor_pages_demo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
